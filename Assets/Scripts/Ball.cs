247d9a1aac85accf9ac7901f88d2833d67722707
Daniel Eriksson <635+daniel@users.noreply.github.com>
2019-10-06 16:16:19 +0200

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+
+    float radius;
+    Vector2 direction;
+
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = transform.localScale.x / 2;
+        gameManager = Object.FindObjectOfType<GameManager>();
+        Spawn();
+    }
+
+    public void Spawn() {
+        transform.position = new Vector3(0,0,0);
+        direction = Vector2.one.normalized;
+        if (Random.Range(0,2) == 1) {
+            direction = -direction;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        
+        // bounce off bottom
+        if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0 ) {
+            direction.y = -direction.y;
+        }
+        // bounce off top
+        if (transform.position.y > GameManager.topRight.y - radius && direction.y > 0) {
+            direction.y = -direction.y;
+        }
+        // going off left
+        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0 ) {
+            gameManager.RightWins();
+            // enabled = false;
+        }        
+        // going off right
+        if (transform.position.x > GameManager.topRight.x - radius && direction.x > 0 ) {
+            gameManager.LeftWins();
+            // enabled = false;
+        }        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Paddle") {
+            GetComponent<AudioSource>().Play();
+            bool isRight = other.GetComponent<Paddle>().isRight;
+            if (isRight && direction.x > 0) {
+                direction.x = -direction.x;
+            }
+            if (!isRight && direction.x < 0) {
+                direction.x = -direction.x;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public Paddle paddle;
+    public Ball ball;
+
+    public TextMeshProUGUI leftScoreText;
+    public TextMeshProUGUI rightScoreText;
+    public TextMeshProUGUI gameMessage;
+    public TextMeshProUGUI restartMessage;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+
+    int leftScore = 0;
+    int rightScore = 0;
+
+    int maxScore = 2; // 10
+
+    Paddle paddle1;
+    Paddle paddle2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        paddle1 = Instantiate(paddle) as Paddle;
+        paddle2 = Instantiate(paddle) as Paddle;
+        ball = Instantiate(ball);
+        paddle1.Init(true);
+        paddle2.Init(false);
+        InitNewGame();
+    }
+
+    public void InitNewGame() {
+        leftScoreText.text = "00";
+        rightScoreText.text = "00";
+        gameMessage.text = "";
+        restartMessage.text = "";
+        ContinueGame();
+    }
+
+    public void LeftWins() {
+        ball.enabled = false;
+        leftScore += 1;
+        UpdateScore();
+        AfterScore();
+    }
+
+    public void RightWins() {
+        ball.enabled = false;
+        rightScore += 1;
+        UpdateScore();
+        AfterScore();
+    }
+
+    void AfterScore() {
+        GetComponent<AudioSource>().Play();
+        if (leftScore >= maxScore) {
+            GameOver("Left player wins!");
+        } else if (rightScore >= maxScore) {
+            GameOver("Right player wins!");
+        } else {
+            ContinueGame();
+        }
+    }
+
+    void ContinueGame() {
+        paddle1.Init(true);
+        paddle2.Init(false);
+        ball.Spawn();
+        ball.enabled = true;
+
+    }
+
+    void GameOver(string message) {
+        gameMessage.text = message;
+        restartMessage.text = "Press space to restart";
+        Time.timeScale = 0;
+        ball.enabled = false;
+    }
+
+    void UpdateScore() {
+        leftScoreText.text = leftScore.ToString("00");
+        rightScoreText.text = rightScore.ToString("00");
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            InitNewGame();
+            Time.timeScale = 1;
+            ball.enabled = true;
+        }
+    }
+
+}
